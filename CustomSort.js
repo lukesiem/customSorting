e372ca34d9705b2235fe8488de381110bb8a70fc
Luke Siemens <lukesiemens@gmail.com>
2017-09-07 16:47:57 +0000

    arranged by name and age

---
+++ CustomSort.js
@@ -0,0 +1,32 @@
+var students = [
+  { id: 1, name: "bruce",    age: 40 },
+  { id: 2, name: "zoidberg", age: 22 },
+  { id: 3, name: "alex",     age: 22 },
+  { id: 4, name: "alex",     age: 30 }
+];
+
+var result = students.sort(function(a,b){
+	var nameA = a.name.toUpperCase();
+	var nameB = b.name.toUpperCase();
+	var ageA = a.age;
+	var ageB = b.age;
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  if (ageA < ageB) {
+    return -1;
+  }
+  if (ageA > ageB) {
+    return 1;
+  }
+	return 0;
+});
+
+  
+
+
+console.log(result)
